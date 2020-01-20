51cec98bbc084970993c2dadf73117c62b188eea
Robert Pastorelli <pastorer@bc.edu>
2020-01-20 13:12:56 -0500

    function for zip checker

---
+++ functions.js
@@ -0,0 +1,13 @@
+function zipCheck(zip){
+    if (zip > 20000){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+/*var inputVal = document.getElementById("zipper").value;
+console.log("v: " + inputVal);
+
+console.log(zipCheck(inputVal));*/
