186109e1aec7916ccfc7d082397f8202e86f7c56
Robert Pastorelli <pastorer@bc.edu>
2020-02-02 12:43:34 -0500

    much work on the banner

--- functions.js
+++ functions.js
@@ -7,6 +7,22 @@ function zipCheck(zip){
     }
 }
 
+
+//display main page header using javascript
+function displayBanner(){
+    var btn = document.getElementById("banner2");
+    var ban = document.getElementById("banner");
+    btn.style.display="block";
+    ban.style.display="block";
+}
+setTimeout(displayBanner, 5000);
+
+document.getElementById("banner2").onclick = function(){
+    var btn = document.getElementById("banner2");
+    var ban = document.getElementById("banner");
+    btn.style.display="none";
+    ban.style.display="none";
+}
 /*var inputVal = document.getElementById("zipper").value;
 console.log("v: " + inputVal);
 
